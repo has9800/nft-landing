a7c4627ea396218e5ebcba5126e7b86b89d40597
ccbagel <hasan.abdi.ahmed97@gmail.com>
2022-07-25 00:21:34 -0600

    footer completed, scrollbars hidden

--- components/stats/Stats.js
+++ components/stats/Stats.js
@@ -1,17 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import statsData from "./statsData";
 import styles from "./stats.module.css";
+import StatsNumbers from "./StatsNumbers";
 
 function Stats() {
   return (
-    <div className={styles.statsContainer}>
-      {statsData.map(({ id, src, text }) => (
-        <div key={id} className={styles.statsCard}>
-          <img src={src} alt="text" className={styles.statsImage} />
-          <p className={styles.statsText}>{text}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <StatsNumbers />
+      <div className={styles.statsContainer}>
+        {statsData.map(({ id, src, text }) => (
+          <div key={id} className={styles.statsCard}>
+            <img src={src} alt="text" className={styles.statsImage} />
+            <p className={styles.statsText}>{text}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
